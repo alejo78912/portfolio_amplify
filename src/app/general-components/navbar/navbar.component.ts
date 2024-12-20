8d3b326809febede8b3da48cd01504b5de116206
Alejandro Valencia Valencia <alejandrovalenciavalencia@Laptop-de-Alejandro.local>
2024-12-19 22:37:12 -0500

    menu issue

--- src/app/general-components/navbar/navbar.component.ts
+++ src/app/general-components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  // Variable para controlar el estado del menú
+  menuOpen = false;
 
+  // Método para alternar el estado del menú
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  // Detectar clic fuera del menú para cerrarlo
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: MouseEvent) {
+    const clickedInside = document.querySelector('.navbar-nav')?.contains(event.target as Node);
+    const clickedToggleButton = document.querySelector('.navbar-toggler')?.contains(event.target as Node);
+
+    if (!clickedInside && !clickedToggleButton && this.menuOpen) {
+      this.menuOpen = false;
+    }
+  }
 }
